db96ef211b28b0bae40e3007ceb1de070bfa23e1
rigobertopo <rperez@cobach.edu.mx>
2022-10-26 16:27:55 -0500

    Inicializar proyecto

---
+++ blog.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi portafolio-blog</title>
+   <link href="css/site.css" rel="stylesheet"/>
+</head>
+<body>
+   <header>
+    <nav id="navbar">
+        <div class="Contenedor">
+             <img src="img/logo.png" alt="Logo" class="Logo"/>
+             <ul class="Texto-btn">
+                 <li ><a href="index.php" class="Seleccionado">Inicio</a></li>
+                 <li><a href="quiensoy.php">Quien soy</a></li>
+                 <li><a href="portafolio.php">PortaFolio</a></li>
+                 <li><a href="blog.php">blog</a></li>
+                 <li><a href="contacto.php">Contactos</a></li>
+             </ul>          
+        </div>
+     </nav> 
+   </header>
+   <section id="Portafolio">
+    <h1>Blog</h1>
+    
+   </section>
+  <footer>
+      <p>Universidad Autónoma de Chiapas</p>
+      <p>Blvd. Belisario Domínguez</p>
+  </footer>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi portafolio-Inicio</title>
+   <link href="css/site.css" rel="stylesheet"/>
+</head>
+<body>
+   <header>
+    <nav id="navbar">
+        <div class="Contenedor">
+             <img src="img/logo.png" alt="Logo" class="Logo"/>
+             <ul class="Texto-btn">
+                 <li ><a href="index.php" class="Seleccionado">Inicio</a></li>
+                 <li><a href="quiensoy.php">Quien soy</a></li>
+                 <li><a href="portafolio.php">PortaFolio</a></li>
+                 <li><a href="blog.php">blog</a></li>
+                 <li><a href="contacto.php">Contactos</a></li>
+             </ul>          
+        </div>
+     </nav> 
+        <div id="MostrarSlider" >
+         <div class="Slider-banner">
+            <h1>Rigoberto Pérez Ovando</h1> 
+            <p class="large">Ingeniero de software</p>
+         </div>
+        </div>
+   </header>
+   <section id="Portafolio">
+    <div class="Contenedor">
+        <div class="portafolio-nav">
+            <ul class="Text-btn">
+                 <li><a href="" class="Seleccionado">Todos</a></li>
+                 <li><a href="">Web</a></li>
+                 <li><a href="">Móvil</a></li>
+                 <li><a href="">Escritorio</a></li>
+                 <li><a href="">Diseños</a></li>
+            </ul>
+        </div>
+    </div>
+    <div class="Portafolio-content">
+        <div class="Caja">
+            <a href="http://">
+                <img src="img/escritorio.jpg" alt="web">
+            </a>               
+        </div>
+        <div class="Caja">
+            <a href="http://">
+                <img src="img/movil.jpg" alt="móvil" >
+            </a>
+        </div>
+        <div class="Caja">
+            <a href="http://">
+                <img src="img/web.jpg" alt="móvil" >
+            </a>
+        </div>
+   </div>
+   </section>
+  <footer>
+      <p>Universidad Autónoma de Chiapas</p>
+      <p>Blvd. Belisario Domínguez</p>
+  </footer>
+</body>
+</html>
